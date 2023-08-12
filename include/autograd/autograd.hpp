6030c14c05d0923f8b62c24512ee7c29318d80ff
Phone Thiha Kyaw <mlsdphonethk@gmail.com>
2023-08-12 18:28:16 +0800

    feat: add addition operator overloads

--- include/autograd/autograd.hpp
+++ include/autograd/autograd.hpp
@@ -11,9 +11,40 @@
 namespace autograd
 {
 
+/**
+ * Forward declarations
+ */
+template <typename T>
+class Variable;
+
+/**
+ * some useful type traits to make our lives easier
+ */
+namespace traits
+{
+
+template <typename T>
+struct isVariableType
+{
+  constexpr static bool value = false;
+};
+
+template <typename T>
+struct isVariableType<Variable<T>>
+{
+  constexpr static bool value = true;
+};
+
+}  // namespace traits
+
 /** A compile-time constant that indicates whether a type is arithmetic */
 template <typename T>
 constexpr bool is_arithmetic = std::is_arithmetic_v<std::remove_cv_t<std::remove_reference_t<T>>>;
+
+/** A compile-time constant that indicates whether a type is Variable */
+template <typename T>
+constexpr bool is_variable =
+    traits::isVariableType<std::remove_cv_t<std::remove_reference_t<T>>>::value;
 
 /**
  * @brief      This class describes a variable.
@@ -21,19 +52,52 @@ constexpr bool is_arithmetic = std::is_arithmetic_v<std::remove_cv_t<std::remove
  * @tparam     T         { description }
  * @tparam     REQUIRES  { description }
  */
-template <typename T, REQUIRES(is_arithmetic<T>)>
+template <typename T>
 class Variable
 {
 public:
   /**
-   * Disable default constructor
+   * Delete all default constructors
    */
   Variable() = delete;
+  Variable(const Variable& other) = delete;
+  Variable& operator=(const Variable& other) = delete;
 
-  /**
-   * @brief TODO
-   */
-  explicit Variable(T&& other) { m_data = other; }
+  template <typename U, REQUIRES(is_arithmetic<U> || is_variable<U>)>
+  Variable(U&& other)
+  {
+    assign(std::forward<U>(other));
+  }
+
+  template <typename U, REQUIRES(is_arithmetic<U> || is_variable<U>)>
+  Variable& operator=(U&& other)
+  {
+    assign(std::forward<U>(other));
+    return *this;
+  }
+
+  //=====================================================================================================================
+  // Addition assignment
+  //=====================================================================================================================
+  template <typename U, REQUIRES(is_arithmetic<U> || is_variable<U>)>
+  Variable& operator+=(U&& other)
+  {
+    addition(std::forward<U>(other));
+    return *this;
+  }
+
+  //=====================================================================================================================
+  // Addition
+  //=====================================================================================================================
+  template <typename U, REQUIRES(is_arithmetic<U> || is_variable<U>)>
+  constexpr auto operator+(U&& other)
+  {
+    Variable tmp(*this);
+    addition(tmp, std::forward<U>(other));
+    return tmp;
+  }
+
+  auto data() const { return m_data; }
 
   /**
    * @brief      Bitwise left shift operator.
@@ -50,7 +114,80 @@ public:
   }
 
 private:
+  template <typename U, typename V>
+  constexpr void assign(Variable<U>& self, V&& other)
+  {
+    static_assert(is_arithmetic<V> || is_variable<V>);
+
+    if constexpr (is_arithmetic<V>)
+    {
+      self.m_data = other;
+    }
+    else if constexpr (is_variable<V>)
+    {
+      self.m_data = other.m_data;
+    }
+  }
+
+  template <typename U, typename V>
+  constexpr void addition(Variable<U>& self, V&& other)
+  {
+    static_assert(is_arithmetic<V> || is_variable<V>);
+
+    if constexpr (is_arithmetic<V>)
+    {
+      self.m_data += other;
+    }
+    else if constexpr (is_variable<V>)
+    {
+      self.m_data += other.m_data;
+    }
+  }
+
+  template <typename U>
+  constexpr void assign(U&& other)
+  {
+    assign(*this, other);
+  }
+
+  template <typename U>
+  constexpr void addition(U&& other)
+  {
+    addition(*this, other);
+  }
+
+private:
   T m_data{};
 };
+
+template <typename T>
+auto data(T&& expr)
+{
+  static_assert(is_arithmetic<T> || is_variable<T>);
+
+  if constexpr (is_variable<T>)
+    return data(expr.data());
+  else
+    return std::forward<T>(expr);
+}
+
+template <typename L, typename R>
+bool operator==(L&& l, R&& r)
+{
+  return data(l) == data(r);
+}
+
+template <typename R, REQUIRES(is_variable<R>)>
+constexpr auto operator+(R&& r)
+{
+  return std::forward<R>(r);  // expression optimization: +(expr) => expr
+}
+
+template <typename L, typename R, REQUIRES(is_arithmetic<L>&& is_variable<R>)>
+constexpr auto operator+(L&& l, R&& r)
+{
+  // CASE: number + variable => variable + number
+  return std::forward<R>(r) + std::forward<L>(l);
+}
 
 }  // namespace autograd
--- tests/test_engine.cpp
+++ tests/test_engine.cpp
@@ -3,11 +3,56 @@
 #include <autograd/autograd.hpp>
 #include <iostream>
 
+using namespace autograd;
+
+TEST(sample_test_case, sample_test)
+{
+  auto a = Variable<int>(1);
+  auto b = Variable<int>(Variable<int>(2));
+  auto c = Variable(a);
+
+  EXPECT_EQ(a, c);
+  EXPECT_TRUE(a == c);
+  EXPECT_FALSE(a == b);
+}
+
+TEST(arithmetic_operations, addition_assignment)
+{
+  // integers
+  auto a = Variable<int>(1);
+  auto b = Variable<int>(2);
+
+  a += b;
+  EXPECT_EQ(a, 3);
+
+  b += 3;
+  EXPECT_EQ(b, 5);
+}
+
+TEST(arithmetic_operations, addition)
+{
+  // integers
+  auto a = Variable<int>(1);
+  auto b = Variable<int>(2);
+  auto c = Variable<int>(3);
+
+  // variable <= variable + number
+  a = a + 4;
+  EXPECT_EQ(a, 5);
+
+  // variable <= number + variable
+  b = 3 + b;
+  EXPECT_EQ(b, 5);
+
+  // variable <= variable + variable
+  EXPECT_EQ(c, 3);
+  c = a + b;
+  EXPECT_EQ(c, 10);
+}
+
 int main(int argc, char** argv)
 {
   testing::InitGoogleTest(&argc, argv);
 
   return RUN_ALL_TESTS();
 }
-
-TEST(sample_test_case, sample_test) { EXPECT_EQ(1, 1); }
